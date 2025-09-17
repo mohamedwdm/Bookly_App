28a6667fed325fefc4d16778cfb458d3a561a7bb
mohamedwdm <wdm46857@gmail.com>
2025-09-17 15:36:57 +0300

    adding sliding text animation to splash view

---
+++ lib/features/splash/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.slidinganimation,
+  });
+
+  final Animation<Offset> slidinganimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidinganimation,
+      builder: (context , _) {
+        return SlideTransition(
+          position: slidinganimation,
+          child: Text('Read Free Books', textAlign: TextAlign.center),
+        );
+      }
+    );
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,18 +1,53 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController; //from 0 to 1 we want more than this
+  late Animation<Offset>
+  slidinganimation; // but controller on ot to make it not to 1 only
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidinganimation = Tween<Offset>(
+      begin: Offset(0, 4),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo ),
-        SizedBox(height: 4,),
-        Text('Read Free Books', textAlign: TextAlign.center,)
+        Image.asset(AssetsData.logo),
+        const SizedBox(height: 4),
+        SlidingText(slidinganimation: slidinganimation),
       ],
     );
   }
