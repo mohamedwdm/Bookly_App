27db7f28f6714009ccba584b40a42eb9c7cebea0
mohamedwdm <wdm46857@gmail.com>
2025-09-21 13:38:51 +0300

    refactor navigation

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,8 @@
-import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/app_routes.dart';
 import 'package:bookly_app/core/utils/assets.dart';
-import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -57,11 +56,12 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
 
   void nivigateToHome() {
     Future.delayed(const Duration(seconds: 2), () {
-      Get.to(
-        () => const HomeView(),
-        duration: kTransitionDuration,
-        transition: Transition.rightToLeftWithFade,
-      );
+      // Get.to(
+      //   () => const HomeView(),
+      //   duration: kTransitionDuration,
+      //   transition: Transition.rightToLeftWithFade,
+      // );
+      GoRouter.of(context).push(AppRoutes.kHomeView);
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/core/utils/app_routes.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -13,14 +12,14 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp.router(
+      routerConfig: AppRoutes.router,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
         textTheme: GoogleFonts.montserratAlternatesTextTheme(ThemeData.dark().textTheme),
       ),
 
       debugShowCheckedModeBanner: false,
-      home: const SplashView(),
     );
   }
 }
