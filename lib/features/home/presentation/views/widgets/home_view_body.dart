fa370603cf58d9f28c8e4fe295d4ae26d8a30149
mohamedwdm <wdm46857@gmail.com>
2025-09-21 12:20:56 +0300

    best seller item 4

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,4 @@
-import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/assets.dart';
+
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_listview_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_appbar.dart';
@@ -11,17 +10,17 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30.0),
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CustomAppBar(),
-          FeaturedBooksListView(),
-          SizedBox(height: 50),
-          Text('Best Seller', style: Styles.textStyle18),
-          SizedBox(height: 20),
-          BestSellerListViewItem(),
+          const CustomAppBar(),
+          const FeaturedBooksListView(),
+          const SizedBox(height: 50),
+          Text('Best Seller', style: Styles.textStyle18.copyWith(color: Colors.white)),
+          const SizedBox(height: 20),
+          const BestSellerListViewItem(),
         ],
       ),
     );
