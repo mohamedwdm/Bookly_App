36083f175771d89a26d31a7c0c3d05332c102de6
mohamedwdm <wdm46857@gmail.com>
2025-09-28 14:46:26 +0300

    add get it and setup service locator

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,24 @@
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setUpServiceLocator() { // to create one instance from what you want and follow dependency injection principle
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(getIt.get<ApiService>()));
+ // example for why  Api Service getIt.registerSingleton<AuthRepoImpl>(HomeRepoImpl(ApiService(Dio())));
+}
+
+
+// we make the get it to avoid this
+//  providers: [
+//         BlocProvider(
+//           create:
+//               (context) => FeaturedBooksCubit(HomeRepoImpl(ApiService(Dio()))),
+//         ),
+//         BlocProvider(
+//           create: (context) => NewstBooksCubit(HomeRepoImpl(ApiService(Dio()))),
+//         ),
+//       ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_routes.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manger/newst_books_cubit/newst_books_cubit.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setUpServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -12,14 +19,33 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRoutes.router,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
-      ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => NewstBooksCubit((getIt.get<HomeRepoImpl>())),
+        ),
+        // BlocProvider(
+        //   create:
+        //       (context) => FeaturedBooksCubit(HomeRepoImpl(ApiService(Dio()))),
+        // ),
+        // BlocProvider(
+        //   create: (context) => NewstBooksCubit(HomeRepoImpl(ApiService(Dio()))),
+        // ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRoutes.router,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          ),
+        ),
 
-      debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
